7627354e5251a09eda15902fc8063d597620fb7e
jlgjunior <jgaribaldijunior@gmail.com>
2020-04-17 09:53:38 -0300

    View configuration

--- src/main/java/controllers/VotingController.java
+++ src/main/java/controllers/VotingController.java
@@ -1,17 +1,25 @@
 package controllers;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import services.VotingService;
 
 @Controller
 public class VotingController {
 
+	private VotingService votingService;
+	
 	public VotingController() {
-		// TODO Auto-generated constructor stub
+		this.votingService = new VotingService(new Date());
 	}
 	
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model uiModel) {
+		uiModel.addAttribute("restaurants", this.votingService.availableRestaurants());
 		return "index";
 	}
 
